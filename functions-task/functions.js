e0036f7ef41215099055aee7013a07712a26bce2
inga kazakeviciute <ingakazakeviciute@ingas-MacBook-Air.local>
2023-09-21 16:13:50 +0300

    functions-update

--- functions-task/functions.js
+++ functions-task/functions.js
@@ -24,20 +24,22 @@ function daysToYears (daysNumber) {
 
 daysToYears(365)
 
-// 5. Funkciją, kuri konvertuoja metus į valandas:
-// 5.1. Funkcija priima vieną parametrą: metų skaičių.
-// 5.2. Funkcija grąžina atsakymą tokiu formatu: 5 metai turi 43800 valandas.
-
 function yearsToHours (yearsNumber) {
     console.log(`${yearsNumber} metai turi ${yearsNumber * 43800 / 5} valandas`)
 }
 
 yearsToHours(5)
 
-// 6. Funkciją, kuri konvertuoja mėnesius į valandas:
-// 6.1. Funkcija priima vieną parametrą: mėnesių skaičių.
-// 6.2. Funkcija grąžina atsakymą tokiu formatu: 3 mėnesiai turi 2190 valandas.
+function monthsToHours (monthsNumber) {
+    console.log(`${monthsNumber} mėnesiai turi ${monthsNumber * 2190 / 3} valandas`)
 
-// 7. Funkciją, kuri konvertuoja mėnesius į minutes:
-// 7.1. Funkcija priima vieną parametrą: mėnesių skaičių.
-// 7.2. Funkcija grąžina atsakymą tokiu formatu: 2 mėnesiai turi 87600 minutes.
+}
+
+monthsToHours(3)
+
+function monthsToMinutes (monthsNumber) {
+    console.log (`${monthsNumber} mėnesiai turi ${monthsNumber * 87600 / 2} minutes`)
+
+}
+
+monthsToMinutes(2)
