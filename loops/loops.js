7930a05520f3746904421017b2d606809147a661
inga kazakeviciute <ingakazakeviciute@ingas-MacBook-Air.local>
2023-10-03 18:06:51 +0300

    loops-update

--- loops/loops.js
+++ loops/loops.js
@@ -109,14 +109,20 @@
     for (let i = 1; i <= 100; i++) {
     if (i % 15 === 0) {
       console.log('fizzBuzz')
-
     }
-      else if (i%3 === 0) {
+    else if (i % 21 === 0) {
+    console.log('fizzBiff')
+    }
+    else if (i%3 === 0) {
         console.log('fizz') 
       }
-      else if (i%5 === 0){
+    else if (i%5 === 0){
         console.log('buzz')
-      }
+    }
+
+    else if (i % 7 === 0){
+      console.log("biff")
+    }
       else 
     console.log (i)
     }
