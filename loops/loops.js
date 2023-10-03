416e98a8e96f20593f3ee49a2ff24cefeb1986c5
inga kazakeviciute <ingakazakeviciute@ingas-MacBook-Air.local>
2023-10-03 17:58:33 +0300

    loops

---
+++ loops/loops.js
@@ -0,0 +1,126 @@
+// function task1() {
+//     for (let i = 1; i <= 100; i++) {
+//       console.log(i*2)
+//     }
+//   }
+  
+//   task1()
+
+  // function task2() {
+  //   for (let i = 1; i <= 100; i++) {
+  //     console.log(i*5)
+  //   }
+  // }
+  
+  // task2()
+
+  // function task3() {
+  //   for (let i = 1; i <= 100; i++) {
+  //     console.log(i + 5)
+  //   }
+  // }
+  
+  // task3()
+
+
+  // function task4() {
+  //   for (let i = 1; i <= 100; i++) {
+  //     console.log(i - 2)
+  //   }
+  // }
+  
+  // task4()
+
+  // function task5() {
+  //   for (let i = 1; i <= 100; i++) {
+  //     console.log(Math.pow (i, 2))
+  //   }
+  // }
+  
+  // task5()
+
+  // function task6() {
+  //   for (let i = 1; i <= 100; i++) {
+  //     console.log(i**3)
+  //   }
+  // }
+  
+  // task6()
+
+  // function task71() {
+  //   for (let i = 100; i >= 1; i--) {
+  //     console.log(i * 2)
+  //   }
+  // }
+  
+  // task71()
+
+  // function task72() {
+  //   for (let i = 100; i >= 1; i--) {
+  //     console.log(i * 5)
+  //   }
+  // }
+  
+  // task72()
+
+  // function task73() {
+  //   for (let i = 100; i >= 1; i--) {
+  //     console.log(i + 5)
+  //   }
+  // }
+  
+  // task73()
+
+  // function task74() {
+  //   for (let i = 100; i >= 1; i--) {
+  //     console.log(i - 2)
+  //   }
+  // }
+  
+  // task74()
+
+  // function task75() {
+  //   for (let i = 100; i >= 1; i--) {
+  //     console.log(i**2)
+  //   }
+  // }
+  
+  // task75()
+
+  // function task76() {
+  //   for (let i = 100; i >= 1; i--) {
+  //     console.log(i**3)
+  //   }
+  // }
+  
+  // task76()
+
+  // function task81() {
+  //   for (let i = 1; i <= 100; i++) {
+  //     let answer = i * 2
+  //     let output = `${i} * 2 = ${answer}`
+  //     console.log(output)
+  //   }
+  // }
+  
+  // task81()
+
+  function fizz() {
+    for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log('fizzBuzz')
+
+    }
+      else if (i%3 === 0) {
+        console.log('fizz') 
+      }
+      else if (i%5 === 0){
+        console.log('buzz')
+      }
+      else 
+    console.log (i)
+    }
+
+  }
+  fizz()
+
